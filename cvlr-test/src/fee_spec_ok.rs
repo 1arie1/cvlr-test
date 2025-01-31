66fa883570c29ea044afc072666f577febc37fee
Arie Gurfinkel <arie@certora.com>
2025-01-31 16:06:12 -0500

    feat: good version of fee spec

---
+++ cvlr-test/src/fee_spec_ok.rs
@@ -0,0 +1,51 @@
+/// Example of a specification
+use cvlr::prelude::*;
+
+/// Function under verification
+fn compute_fee(amount: u64, fee_bps: u16) -> Result<u64,()> {
+    if amount > 0 {
+        Ok((amount as u128)
+            .checked_mul(fee_bps as u128)
+            .and_then(|x| Some(x.div_ceil(10_000)))
+            .ok_or(())?
+            .try_into()
+            .map_err(|_| ())?)
+    } else {
+        Err(())
+    }
+}
+
+
+
+
+#[rule]
+pub fn rule_fee_sanity_ok() {
+    compute_fee(nondet(), nondet()).unwrap();
+    cvlr_satisfy!(true);
+}
+
+#[rule]
+pub fn rule_fee_assessed_ok() {
+    let amt: u64 = nondet();
+    let fee_bps: u16 = nondet();
+    cvlr_assume!(fee_bps <= 10_000);
+    let fee = compute_fee(amt, fee_bps).unwrap();
+    clog!(amt, fee_bps, fee);
+    cvlr_assert!(fee <= amt);
+    if fee_bps > 0 {
+        cvlr_assert!(fee > 0);
+    }
+}
+
+#[rule]
+pub fn rule_fee_liveness_ok() {
+    let amt: u64 = nondet();
+    let fee_bps: u16 = nondet();
+    cvlr_assume!(fee_bps <= 10_000);
+    let fee = compute_fee(amt, fee_bps);
+    clog!(amt, fee_bps, fee);
+    if fee.is_err() {
+        cvlr_assert!(amt == 0);
+    }
+    cvlr_vacuity_check!();
+}
--- cvlr-test/src/lib.rs
+++ cvlr-test/src/lib.rs
@@ -1,18 +1,5 @@
 
 mod asserts;
 mod fee_spec;
+mod fee_spec_ok;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
